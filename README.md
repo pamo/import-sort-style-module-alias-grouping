f0fb4c8c34e1af236a51308859d1a0c78d21926e
Pamela Ocampo <pamela.ocampo@gmail.com>
2019-10-28 13:39:56 -0700

    update sort by modules with @ imports

--- README.md
+++ README.md
@@ -4,36 +4,39 @@ A style for [import-sort](https://github.com/renke/import-sort) that is focused
 on modules and aliases.
 
 ```js
+// Third-party aliased modules by name
+import { NgModule } from '@angular/core';
+
 // Absolute modules with side effects (not sorted because order may matter)
-import "a";
-import "c";
-import "b";
+import 'a';
+import 'c';
+import 'b';
 
 // Relative modules with side effects (not sorted because order may matter)
-import "./a";
-import "./c";
-import "./b";
+import './a';
+import './c';
+import './b';
 
 // Modules from the Node.js "standard" library sorted by name
-import {readFile, writeFile} from "fs";
-import * as path from "path";
+import { readFile, writeFile } from 'fs';
+import * as path from 'path';
 
 // Third-party modules sorted by name
-import aa from "aa";
-import bb from "bb";
-import cc from "cc";
+import aa from 'aa';
+import bb from 'bb';
+import cc from 'cc';
 
 // First-party aliased modules by name
-import aaa from "@app/aaa";
-import bbb from "@app/bbb";
-import aaaa from "@app/bar/aaaa";
-import bbbb from "@app/foo/bbbb";
+import aaa from '@app/aaa';
+import bbb from '@app/bbb';
+import aaaa from '@app/bar/aaaa';
+import bbbb from '@app/foo/bbbb';
 
-```// First-party modules sorted by "relative depth" and then by name
-import aaa from "../../aaa";
-import bbb from "../../bbb";
-import aaaa from "../aaaa";
-import bbbb from "../bbbb";
-import aaaaa from "./aaaaa";
-import bbbbb from "./bbbbb";
+// First-party modules sorted by "relative depth" and then by name
+import aaa from '../../aaa';
+import bbb from '../../bbb';
+import aaaa from '../aaaa';
+import bbbb from '../bbbb';
+import aaaaa from './aaaaa';
+import bbbbb from './bbbbb';
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {IStyleAPI, IStyleItem} from "import-sort-style";
+import { IStyleAPI, IStyleItem } from 'import-sort-style';
 
 export default function(styleApi: IStyleAPI): IStyleItem[] {
   const {
@@ -6,46 +6,66 @@ export default function(styleApi: IStyleAPI): IStyleItem[] {
     and,
     dotSegmentCount,
     hasNoMember,
+    hasOnlyNamespaceMember,
     isAbsoluteModule,
     isNodeModule,
     isRelativeModule,
+    member,
     moduleName,
     naturally,
-    unicode,
+    not,
+    unicode
   } = styleApi;
+  const startsWithAt = (string: string) => string.startsWith('@');
 
   return [
-    // import "foo"
-    {match: and(hasNoMember, isAbsoluteModule)},
-    {separator: true},
-
-    // import "./foo"
-    {match: and(hasNoMember, isRelativeModule)},
-    {separator: true},
+    // import … from "@angular/core";
+    {
+      match: and(isNodeModule, moduleName(startsWithAt)),
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true },
 
     // import … from "fs";
     {
       match: isNodeModule,
       sort: moduleName(naturally),
-      sortNamedMembers: alias(unicode),
+      sortNamedMembers: alias(unicode)
     },
-    {separator: true},
+    { separator: true },
+
+    // import "foo"
+    { match: and(hasNoMember, isAbsoluteModule) },
+    { separator: true },
+
+    // import "./foo"
+    { match: and(hasNoMember, isRelativeModule) },
+    { separator: true },
+
+    // import … from "@foo/bar";
+    {
+      match: and(moduleName(startsWithAt), not(hasOnlyNamespaceMember)),
+      sort: member(naturally),
+      sortNamedMembers: alias(unicode)
+    },
+    { separator: true },
 
     // import … from "foo";
     {
       match: isAbsoluteModule,
       sort: moduleName(naturally),
-      sortNamedMembers: alias(unicode),
+      sortNamedMembers: alias(unicode)
     },
-    {separator: true},
+    { separator: true },
 
     // import … from "./foo";
     // import … from "../foo";
     {
       match: isRelativeModule,
       sort: [dotSegmentCount, moduleName(naturally)],
-      sortNamedMembers: alias(unicode),
+      sortNamedMembers: alias(unicode)
     },
-    {separator: true},
+    { separator: true }
   ];
 }
