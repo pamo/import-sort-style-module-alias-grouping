1befa8171d42f5ed243b6c0e77320d8382ba6988
Pamela Ocampo <pamela.ocampo@gmail.com>
2019-10-28 16:05:35 -0700

    0.0.2

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import { IStyleAPI, IStyleItem } from 'import-sort-style';
-export default function (styleApi: IStyleAPI): IStyleItem[];
+declare const _default: (styleApi: IStyleAPI, file?: string | undefined, options?: any) => IStyleItem[];
+export default _default;
 //# sourceMappingURL=index.d.ts.map
